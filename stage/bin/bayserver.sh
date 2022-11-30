58ff50ae8531dfb540f2d4780505b1f76d33c27c
Michisuke-P <michisukep@gmail.com>
2022-11-30 23:09:45 +0900

    Modifies startup scripts

--- stage/bin/bayserver.sh
+++ stage/bin/bayserver.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 base=`dirname $0`
-base=`realpath $base`
-
-bhome=`realpath $base/..`
-export BSERV_HOME=$bhome
-
-dockers=${bhome}/docker
-
-PYTHONPATH=${bhome}/core
-
-if [ -e $dockers ]; then
-  for f in ${dockers}/*; do
-    PYTHONPATH=$PYTHONPATH:${f}
-  done
-fi
-
 
 args=$*
 daemon=
 for arg in $args; do
-  if [ "$arg" == "-daemon" ]; then
+  if [ "$arg" = "-daemon" ]; then
     daemon=1
   fi
 done
@@ -30,8 +15,7 @@ if [ "$pyver" != "3" ]; then
    pycmd=python3
 fi
 
-export PYTHONPATH
-if [ "$daemon" == 1 ]; then
+if [ "$daemon" = 1 ]; then
    $pycmd $base/bootstrap.py $* < /dev/null  > /dev/null 2>&1 &
 else
    $pycmd $base/bootstrap.py $* 
--- stage/bin/bootstrap.py
+++ stage/bin/bootstrap.py
@@ -3,6 +3,8 @@ import sys
 
 base_dir = os.path.dirname(__file__)
 bserv_home = os.path.dirname(base_dir)
+if bserv_home == "":
+    bserv_home = "."
 os.environ["BSERV_HOME"] = bserv_home
 
 core = os.path.join(bserv_home, 'lib', 'core')
