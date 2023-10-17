eb4703447a9cb1f91852d0925c1d4a8b070ca7ee
Michisuke-P <michisukep@gmail.com>
2023-10-17 23:18:39 +0900

    [core] Simplifies the contents of the method

--- packages/bayserver-core/bayserver_core/util/url_encoder.py
+++ packages/bayserver-core/bayserver_core/util/url_encoder.py
@@ -1,17 +1,7 @@
 class URLEncoder:
-    ENCODED_TILDE_BYTES = "%7E".encode("us-ascii")
+    TILDE_STRING = "~"
+    ENCODED_TILDE_STRING = "%7E"
 
     @classmethod
     def encode_tilde(cls, url):
-        if url.find("~") == -1:
-            return url
-
-        buf = bytearray()
-        for c in url:
-            if c == "~":
-                for n in cls.ENCODED_TILDE_BYTES:
-                    buf.append(n)
-            else:
-                buf.append(ord(c))
-
-        return buf.decode("us-ascii")
+        return url.replace(cls.TILDE_STRING, cls.ENCODED_TILDE_STRING)
